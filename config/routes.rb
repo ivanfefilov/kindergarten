c4e6579efa4c7a269bd656ae537af6a62a6b01f9
Ivan Fefilov <ivfefilov@gmail.com>
2016-07-02 15:01:58 +0400

    add admin namespaces

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
-  #devise_for :users
+  devise_for :users
   
   root 'pages#home' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  namespace :admin do
+    get '' => 'dashboard#index'
+    resources :posts, :users
+  end  
 end
