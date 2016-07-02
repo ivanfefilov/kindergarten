f219415586bd19326d88a1849308aec0ca32ffd1
Ivan Fefilov <ivfefilov@gmail.com>
2016-07-02 15:52:03 +0400

    update admin layout

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   namespace :admin do
-    get '' => 'dashboard#index'
+    get '' => 'dashboard#index', as: 'root'
     resources :posts, :users
   end  
 end
