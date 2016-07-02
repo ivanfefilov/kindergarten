414567e7d0d93a8fd6e6a3397083031dfae44e30
Ivan Fefilov <ivfefilov@gmail.com>
2016-07-02 15:11:39 +0400

    add admin flag, first name, last name to users

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,4 +1,11 @@
 class Admin::AdminController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :check_admin
   layout 'admin'
+  
+  private
+  
+  def check_admin
+    current_user.is_admin ? true : (render text: 'У вас нет прав для редактирования этого раздела!')
+  end  
 end  
