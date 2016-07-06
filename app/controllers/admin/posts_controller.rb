9ceca1871dba6f4c6df77fcd15af4c43c49ea150
Ivan Fefilov <ivfefilov@gmail.com>
2016-07-06 12:33:28 +0400

    allow to manage posts in admin

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,3 +1,73 @@
-class Admin::PostsController < Admin::AdminController
+class Admin::PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  layout 'admin'
 
+  # GET /admin/posts
+  # GET /admin/posts.json
+  def index
+    @posts = Post.all
+  end
+
+  # GET /admin/posts/1
+  # GET /admin/posts/1.json
+  def show
+  end
+
+  # GET /admin/posts/new
+  def new
+    @post = Post.new
+  end
+
+  # GET /admin/posts/1/edit
+  def edit
+  end
+
+  # POST /admin/posts
+  # POST /admin/posts.json
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to [:admin, @post], notice: 'Новость успешно создана.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /admin/posts/1
+  # PATCH/PUT /admin/posts/1.json
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to [:admin, @post], notice: 'Новость успешно обновлена.' }
+        format.json { render :show, status: :ok, location: [:admin, @post] }
+      else
+        format.html { render :edit }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /admin/posts/1
+  # DELETE /admin/posts/1.json
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_posts_url, notice: 'Новость успешно удалена' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
