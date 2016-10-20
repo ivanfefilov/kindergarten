c4b34f4767c9e1c3d7558e34495e4de8f0154b4c
Ivan Fefilov <ivfefilov@gmail.com>
2016-10-20 16:43:38 +0400

    mailer setting

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,11 @@ module Kindergarten
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :ru
+    
+    # Default e-mail address which will be shown in the "from" devise emails, initializers/devise.rb,
+    config.noreply = 'noreply@Kindergarten.ru'
+
+    # Default host for action mailer, initializers/mailer.rb
+    config.host = 'localhost:5000'
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.host = 'localhost:5000'
 end
